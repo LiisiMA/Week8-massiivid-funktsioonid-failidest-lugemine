46cc25bd0c0f8daafe98899be2143480ee3f02c2
Liisi <lmarkus@tktk.ee>
2022-01-04 19:34:03 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomC;
+            randomC = GetRandomFromFile("chuck.txt");
+
+            Console.WriteLine($"{randomC}");
+
+            string filePath = @"C:\Users\liisi\samples\chuck.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\liisi\samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
